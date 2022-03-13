183a33aea1b23745ee5bb505c8452a7c602c5490
Yuriy E. Minakov <Y.Minakov@pulkovo-airport.com>
2022-03-13 21:51:01 +0300

    Create ObjectMapperConfig

---
+++ src/main/java/com/minakov/yandexraspintegration/config/ObjectMapperConfig.java
@@ -0,0 +1,30 @@
+package com.minakov.yandexraspintegration.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        final ObjectMapper result = new ObjectMapper();
+        result.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        result.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        result.configure(DeserializationFeature.ACCEPT_FLOAT_AS_INT, false);
+        result.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        result.setPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy());
+        result.registerModule(new Jdk8Module());
+        result.registerModule(new JavaTimeModule());
+        result.registerModule(new ParameterNamesModule());
+
+        return result;
+    }
+}
