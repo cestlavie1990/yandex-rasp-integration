25bf82b2958cbe7572bde954b51a06a4a729eead
Yuriy E. Minakov <Y.Minakov@pulkovo-airport.com>
2022-03-15 20:41:44 +0300

    Add test helper

---
+++ src/test/java/com/minakov/yandexraspintegration/it/helper/AbstractTestHelper.java
@@ -0,0 +1,36 @@
+package com.minakov.yandexraspintegration.it.helper;
+
+import com.minakov.yandexraspintegration.model.JpaEntity;
+import com.minakov.yandexraspintegration.repository.GenericRepository;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import javax.annotation.Nullable;
+import lombok.NonNull;
+import org.springframework.transaction.annotation.Transactional;
+
+public abstract class AbstractTestHelper<ID, E extends JpaEntity<ID>> {
+    @Transactional(readOnly = true)
+    public @Nullable
+    <T> T get(@NonNull final ID id, @NonNull final Function<E, T> mapper) {
+        return mapper.apply(getRepository().findById(id).orElseThrow());
+    }
+
+    @Transactional(readOnly = true)
+    public boolean existsById(@NonNull final ID id) {
+        return getRepository().existsById(id);
+    }
+
+    @Transactional
+    public void update(@NonNull final ID id, @NonNull final Consumer<E> consumer) {
+        final var entity = Objects.requireNonNull(get(id, Function.identity()));
+
+        consumer.accept(entity);
+
+        getRepository().save(entity);
+    }
+
+    public abstract @NonNull ID create();
+
+    protected abstract @NonNull GenericRepository<E, ID> getRepository();
+}
---
+++ src/test/java/com/minakov/yandexraspintegration/it/helper/CountryTestHelper.java
@@ -0,0 +1,34 @@
+package com.minakov.yandexraspintegration.it.helper;
+
+import com.minakov.yandexraspintegration.model.CountryEntity;
+import com.minakov.yandexraspintegration.model.embedded.CodeEmbedded;
+import com.minakov.yandexraspintegration.repository.CountryRepository;
+import com.minakov.yandexraspintegration.repository.GenericRepository;
+import java.util.UUID;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class CountryTestHelper extends AbstractTestHelper<UUID, CountryEntity> {
+    @Autowired
+    private CountryRepository repository;
+
+    public static String DEFAULT_TITLE = "countryTitle";
+    public static CodeEmbedded DEFAULT_CODE =
+            CodeEmbedded.builder().yandexCode("countryYandexCode").esrCode("countryEsrCode").build();
+
+    @Transactional
+    @Override
+    public @NonNull UUID create() {
+        return repository.save(CountryEntity.builder().code(DEFAULT_CODE).title(DEFAULT_TITLE).build()).getId();
+    }
+
+    @Override
+    protected @NonNull GenericRepository<CountryEntity, UUID> getRepository() {
+        return repository;
+    }
+}
