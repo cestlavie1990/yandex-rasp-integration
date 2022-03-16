82c722a79d4879e99530ad6dcc85c309a7898eb5
Yuriy E. Minakov <Y.Minakov@pulkovo-airport.com>
2022-03-16 20:31:47 +0300

    Add CountryMapper

--- src/main/java/com/minakov/yandexraspintegration/service/country/CountryMapper.java
+++ src/main/java/com/minakov/yandexraspintegration/service/country/CountryMapper.java
@@ -2,7 +2,9 @@ package com.minakov.yandexraspintegration.service.country;
 
 import com.minakov.yandexraspintegration.controller.graphql.type.country.Country;
 import com.minakov.yandexraspintegration.model.CountryEntity;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
@@ -10,7 +12,11 @@ import org.mapstruct.factory.Mappers;
 public interface CountryMapper {
     CountryMapper INSTANCE = Mappers.getMapper(CountryMapper.class);
 
-    Country toDto(final CountryEntity country);
+    Country toDto(final CountryEntity source);
+
+    default List<Country> toDto(final List<CountryEntity> sources) {
+        return sources.stream().map(this::toDto).collect(Collectors.toList());
+    }
 
     default String map(final UUID id) {
         return id == null ? null : id.toString();
--- src/test/java/com/minakov/yandexraspintegration/unit/service/country/CountryMapperTest.java
+++ src/test/java/com/minakov/yandexraspintegration/unit/service/country/CountryMapperTest.java
@@ -2,10 +2,12 @@ package com.minakov.yandexraspintegration.unit.service.country;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.minakov.yandexraspintegration.model.CountryEntity;
 import com.minakov.yandexraspintegration.model.embedded.CodeEmbedded;
 import com.minakov.yandexraspintegration.service.country.CountryMapper;
+import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +15,7 @@ class CountryMapperTest {
     private static final CountryMapper MAPPER = CountryMapper.INSTANCE;
 
     @Test
-    void map_withoutNull() {
+    void toDto_withoutNull() {
         final var entity = CountryEntity.builder()
                 .id(UUID.randomUUID())
                 .title("test-title")
@@ -29,7 +31,7 @@ class CountryMapperTest {
     }
 
     @Test
-    void map_withNull() {
+    void toDto_withNull() {
         final var entity = CountryEntity.builder()
                 .id(UUID.randomUUID())
                 .title("test-title")
@@ -42,5 +44,29 @@ class CountryMapperTest {
         assertEquals(entity.getTitle(), dto.getTitle());
         assertNull(dto.getCode().getYandexCode());
         assertNull(dto.getCode().getEsrCode());
+    }
+
+    @Test
+    void toDto_list() {
+        final var entity1 = CountryEntity.builder()
+                .id(UUID.randomUUID())
+                .title("test-title1")
+                .code(CodeEmbedded.builder().yandexCode("test-yandex-code1").esrCode("test-esr-code1").build())
+                .build();
+        final var entity2 = CountryEntity.builder()
+                .id(UUID.randomUUID())
+                .title("test-title2")
+                .code(CodeEmbedded.builder().build())
+                .build();
+
+        final var dto1 = MAPPER.toDto(entity1);
+        final var dto2 = MAPPER.toDto(entity2);
+
+        final var results = MAPPER.toDto(List.of(entity1, entity2));
+
+        assertEquals(2, results.size());
+
+        assertTrue(results.stream().anyMatch(result -> result.equals(dto1)));
+        assertTrue(results.stream().anyMatch(result -> result.equals(dto2)));
     }
 }
