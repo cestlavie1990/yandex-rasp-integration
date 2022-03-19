f0c9364e8a70ba3f5ce6ac86b6c0ef0389ac19cb
Yuriy E. Minakov <Y.Minakov@pulkovo-airport.com>
2022-03-19 17:22:55 +0300

    Update criteria filter

--- src/main/java/com/minakov/yandexraspintegration/controller/graphql/input/filter/StringCriteria.java
+++ src/main/java/com/minakov/yandexraspintegration/controller/graphql/input/filter/StringCriteria.java
@@ -1,6 +1,5 @@
 package com.minakov.yandexraspintegration.controller.graphql.input.filter;
 
-import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.lang.Nullable;
@@ -9,7 +8,7 @@ import org.springframework.lang.Nullable;
 @Builder
 public class StringCriteria implements ICriteria {
     @Nullable
-    private final String like;
+    private final StringCriteriaValue in;
     @Nullable
-    private final List<String> in;
+    private final StringCriteriaValue like;
 }
--- src/main/java/com/minakov/yandexraspintegration/controller/graphql/input/filter/StringCriteriaValue.java
+++ src/main/java/com/minakov/yandexraspintegration/controller/graphql/input/filter/StringCriteriaValue.java
@@ -1,5 +1,6 @@
 package com.minakov.yandexraspintegration.controller.graphql.input.filter;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,12 +9,11 @@ import lombok.NonNull;
 @Getter
 @Builder
 public class StringCriteriaValue {
+    @Builder.Default
     @NonNull
-    private final List<String> values;
+    private final List<String> values = new ArrayList<>();
+
     @Builder.Default
     @NonNull
     private final Boolean inverse = Boolean.FALSE;
-    @Builder.Default
-    @NonNull
-    private final CriteriaOperator operator = CriteriaOperator.AND;
 }
