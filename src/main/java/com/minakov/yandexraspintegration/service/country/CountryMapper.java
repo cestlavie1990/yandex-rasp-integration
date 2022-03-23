7a55a4450dcc232baa632ab83c789eceac6aade0
Yuriy E. Minakov <Y.Minakov@pulkovo-airport.com>
2022-03-23 18:56:39 +0300

    Fix CountryMapper

--- src/main/java/com/minakov/yandexraspintegration/service/country/CountryMapper.java
+++ src/main/java/com/minakov/yandexraspintegration/service/country/CountryMapper.java
@@ -3,15 +3,18 @@ package com.minakov.yandexraspintegration.service.country;
 import com.minakov.yandexraspintegration.controller.graphql.type.country.Country;
 import com.minakov.yandexraspintegration.model.CountryEntity;
 import com.minakov.yandexraspintegration.service.IMapper;
-import java.util.UUID;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CountryMapper extends IMapper<CountryEntity, Country> {
     CountryMapper INSTANCE = Mappers.getMapper(CountryMapper.class);
 
-    default String map(final UUID id) {
-        return id == null ? null : id.toString();
-    }
+    @Override
+    @Mappings({
+            @Mapping(target = "id", expression = "java(source.getId().toString())")
+    })
+    Country toDto(final CountryEntity source);
 }
