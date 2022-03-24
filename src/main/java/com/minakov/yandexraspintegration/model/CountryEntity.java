d1fb821e27d8c568c5a4e81102338e7209b52b04
Yuriy E. Minakov <Y.Minakov@pulkovo-airport.com>
2022-03-24 09:50:14 +0300

    Add linked collection of entities

--- src/main/java/com/minakov/yandexraspintegration/model/CountryEntity.java
+++ src/main/java/com/minakov/yandexraspintegration/model/CountryEntity.java
@@ -1,12 +1,17 @@
 package com.minakov.yandexraspintegration.model;
 
 import com.minakov.yandexraspintegration.model.embedded.CodeEmbedded;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -36,4 +41,10 @@ public class CountryEntity extends AbstractAuditEntity implements IEntity<UUID> 
 
     @Column(nullable = false)
     private String title;
+
+    @Builder.Default
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "country", cascade = CascadeType.ALL)
+    private List<RegionEntity> regions = new ArrayList<>();
 }
--- src/main/java/com/minakov/yandexraspintegration/model/SettlementEntity.java
+++ src/main/java/com/minakov/yandexraspintegration/model/SettlementEntity.java
@@ -1,7 +1,10 @@
 package com.minakov.yandexraspintegration.model;
 
 import com.minakov.yandexraspintegration.model.embedded.CodeEmbedded;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -10,6 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -48,6 +52,12 @@ public class SettlementEntity extends AbstractAuditEntity implements IEntity<UUI
 
     @Column(nullable = false)
     private String title;
+
+    @Builder.Default
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "settlement", cascade = CascadeType.ALL)
+    private List<StationEntity> stations = new ArrayList<>();
 
     public void setRegion(RegionEntity region) {
         this.region = region;
