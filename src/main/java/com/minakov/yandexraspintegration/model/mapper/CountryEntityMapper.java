5aa3d9fd42b4b7432ce08a67107109c8e12c60d6
Yuriy E. Minakov <Y.Minakov@pulkovo-airport.com>
2022-03-26 12:27:22 +0300

    Add RegionEntityMapperTest

--- src/main/java/com/minakov/yandexraspintegration/model/mapper/CountryEntityMapper.java
+++ src/main/java/com/minakov/yandexraspintegration/model/mapper/CountryEntityMapper.java
@@ -3,6 +3,7 @@ package com.minakov.yandexraspintegration.model.mapper;
 import com.minakov.yandexraspintegration.feign.dto.yandex.rasp.CountryDto;
 import com.minakov.yandexraspintegration.model.CountryEntity;
 import com.minakov.yandexraspintegration.service.IMapper;
+import com.minakov.yandexraspintegration.util.MapperUtils;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
@@ -11,15 +12,15 @@ import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(uses = {RegionEntityMapper.class, CodeEmbeddedMapper.class}, builder = @Builder(disableBuilder = true))
+@Mapper(uses = {RegionEntityMapper.class, CodeEmbeddedMapper.class, MapperUtils.class},
+        builder = @Builder(disableBuilder = true))
 public interface CountryEntityMapper extends IMapper<CountryDto, CountryEntity> {
     CountryEntityMapper INSTANCE = Mappers.getMapper(CountryEntityMapper.class);
 
     @Override
     @Mappings({
             @Mapping(target = "id", ignore = true), @Mapping(target = "code", source = "codes"),
-            @Mapping(target = "title",
-                    expression = "java(org.apache.commons.lang3.StringUtils.defaultIfBlank(source.getTitle(), null))")
+            @Mapping(qualifiedByName = "NullIfBlank", target = "title")
     })
     CountryEntity map(final CountryDto source);
 
---
+++ src/test/java/com/minakov/yandexraspintegration/unit/model/mapper/CountryEntityMapperTest.java
@@ -0,0 +1,111 @@
+package com.minakov.yandexraspintegration.unit.model.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.minakov.yandexraspintegration.feign.dto.yandex.rasp.CodeDto;
+import com.minakov.yandexraspintegration.feign.dto.yandex.rasp.CountryDto;
+import com.minakov.yandexraspintegration.feign.dto.yandex.rasp.RegionDto;
+import com.minakov.yandexraspintegration.feign.dto.yandex.rasp.SettlementDto;
+import com.minakov.yandexraspintegration.feign.dto.yandex.rasp.StationDto;
+import com.minakov.yandexraspintegration.model.RegionEntity;
+import com.minakov.yandexraspintegration.model.mapper.CountryEntityMapper;
+import com.minakov.yandexraspintegration.model.mapper.RegionEntityMapper;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CountryEntityMapperTest {
+    private static final CountryEntityMapper MAPPER = CountryEntityMapper.INSTANCE;
+
+    private static RegionDto REGION_DTO;
+    private static RegionEntity REGION_ENTITY;
+
+    @BeforeEach
+    void beforeEach() {
+        final var station = StationDto.builder()
+                .title("station-title")
+                .codes(CodeDto.builder().esrCode("station-esr-code").yandexCode("station-yandex-code").build())
+                .direction("station-direction")
+                .stationType("station-station-type")
+                .transportType("station-transport-type")
+                .latitude(100.001)
+                .longitude(-100.0001)
+                .build();
+
+        final var settlement = SettlementDto.builder()
+                .title("settlement-title")
+                .codes(CodeDto.builder().esrCode("settlement-esr-code").yandexCode("settlement-yandex-code").build())
+                .stations(List.of(station))
+                .build();
+
+        REGION_DTO = RegionDto.builder()
+                .title("region-title")
+                .codes(CodeDto.builder().esrCode("region-esr-code").yandexCode("region-yandex-code").build())
+                .settlements(List.of(settlement))
+                .build();
+
+        REGION_ENTITY = RegionEntityMapper.INSTANCE.map(REGION_DTO);
+    }
+
+    @Test
+    void toDto_withoutNull() {
+        final var source = CountryDto.builder()
+                .title("title")
+                .codes(CodeDto.builder().esrCode("esr-code").yandexCode("yandex-code").build())
+                .regions(List.of(REGION_DTO))
+                .build();
+
+        final var target = MAPPER.map(source);
+
+        assertEquals(source.getTitle(), target.getTitle());
+        assertEquals(source.getCodes().getEsrCode(), target.getCode().getEsrCode());
+        assertEquals(source.getCodes().getYandexCode(), target.getCode().getYandexCode());
+
+        assertNotNull(source.getRegions());
+        assertEquals(source.getRegions().size(), target.getRegions().size());
+        assertEquals(REGION_ENTITY, target.getRegions().get(0));
+        assertEquals(target, target.getRegions().get(0).getCountry());
+    }
+
+    @Test
+    void toDto_withNull() {
+        final var source = CountryDto.builder().build();
+
+        final var target = MAPPER.map(source);
+
+        assertNull(target.getCode());
+        assertNull(target.getTitle());
+        assertNull(source.getRegions());
+    }
+
+    @Test
+    void toDto_withBlankValues() {
+        final var source = CountryDto.builder().title("").regions(List.of()).build();
+
+        final var target = MAPPER.map(source);
+
+        assertNull(target.getCode());
+        assertNull(target.getTitle());
+
+        assertNotNull(source.getRegions());
+        assertTrue(source.getRegions().isEmpty());
+    }
+
+    @Test
+    void toDto_list() {
+        final var source = CountryDto.builder()
+                .title("title")
+                .codes(CodeDto.builder().esrCode("esr-code").yandexCode("yandex-code").build())
+                .regions(List.of(REGION_DTO))
+                .build();
+
+        final var target = MAPPER.map(source);
+        final var targets = MAPPER.map(List.of(source));
+
+        assertEquals(1, targets.size());
+        assertTrue(targets.stream().anyMatch(result -> result.equals(target)));
+    }
+}
