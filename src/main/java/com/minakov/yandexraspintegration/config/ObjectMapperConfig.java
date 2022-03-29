482996b56f659d148027d8923ec6116eb467f035
Yuriy E. Minakov <Y.Minakov@pulkovo-airport.com>
2022-03-29 16:19:16 +0300

    Fix feign mapper config

--- src/main/java/com/minakov/yandexraspintegration/config/ObjectMapperConfig.java
+++ src/main/java/com/minakov/yandexraspintegration/config/ObjectMapperConfig.java
@@ -3,7 +3,6 @@ package com.minakov.yandexraspintegration.config;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -14,7 +13,6 @@ import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class ObjectMapperConfig {
-
     @Primary
     @Bean
     public ObjectMapper objectMapper() {
@@ -23,21 +21,6 @@ public class ObjectMapperConfig {
         result.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
         result.configure(DeserializationFeature.ACCEPT_FLOAT_AS_INT, false);
         result.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        result.registerModule(new Jdk8Module());
-        result.registerModule(new JavaTimeModule());
-        result.registerModule(new ParameterNamesModule());
-
-        return result;
-    }
-
-    @Bean
-    public ObjectMapper snakeCaseMapper() {
-        final ObjectMapper result = new ObjectMapper();
-        result.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        result.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-        result.configure(DeserializationFeature.ACCEPT_FLOAT_AS_INT, false);
-        result.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        result.setPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy());
         result.registerModule(new Jdk8Module());
         result.registerModule(new JavaTimeModule());
         result.registerModule(new ParameterNamesModule());
--- src/main/java/com/minakov/yandexraspintegration/config/feign/YandexRaspConfig.java
+++ src/main/java/com/minakov/yandexraspintegration/config/feign/YandexRaspConfig.java
@@ -1,6 +1,13 @@
 package com.minakov.yandexraspintegration.config.feign;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import com.minakov.yandexraspintegration.config.properties.YandexRaspProperties;
 import com.minakov.yandexraspintegration.feign.YandexRaspClient;
 import feign.Feign;
@@ -11,25 +18,39 @@ import feign.okhttp.OkHttpClient;
 import feign.slf4j.Slf4jLogger;
 import java.util.concurrent.TimeUnit;
 import lombok.NonNull;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class YandexRaspConfig {
-
     @Bean
-    public YandexRaspClient yandexRaspClient(@NonNull @Qualifier("snakeCaseMapper") final ObjectMapper objectMapper,
-            @NonNull final YandexRaspProperties prop) {
+    public YandexRaspClient yandexRaspClient(@NonNull final YandexRaspProperties prop) {
+        final var mapper = createMapper();
+
         return Feign.builder()
                 .client(new OkHttpClient())
-                .encoder(new JacksonEncoder(objectMapper))
-                .decoder(new JacksonDecoder(objectMapper))
+                .encoder(new JacksonEncoder(mapper))
+                .decoder(new JacksonDecoder(mapper))
                 .options(new Request.Options(prop.getConnectTimeout(), TimeUnit.MILLISECONDS, prop.getReadTimeout(),
                         TimeUnit.MILLISECONDS, true))
                 .logger(new Slf4jLogger(YandexRaspClient.class))
                 .contract(new SpringMvcContract())
                 .target(YandexRaspClient.class, prop.getUrl());
+    }
+
+    private ObjectMapper createMapper() {
+        final var result = new ObjectMapper();
+        result.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        result.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        result.configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, false);
+        result.configure(DeserializationFeature.ACCEPT_FLOAT_AS_INT, false);
+        result.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        result.setPropertyNamingStrategy(new PropertyNamingStrategies.SnakeCaseStrategy());
+        result.registerModule(new Jdk8Module());
+        result.registerModule(new JavaTimeModule());
+        result.registerModule(new ParameterNamesModule());
+
+        return result;
     }
 }
